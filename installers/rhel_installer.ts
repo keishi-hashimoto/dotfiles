fa0d1eca5f9728869d278009a293c12aa4531747
keishi hashimoto <keishi04hrikzira@gmail.com>
2025-05-17 05:10:42 -0400

    ✨ RHEL 用のインストーラに Git コマンドのインストール処理を追加

--- installers/rhel_installer.ts
+++ installers/rhel_installer.ts
@@ -1,8 +1,12 @@
 import { BaseInstaller } from "./index.ts";
 import { setupFish } from "../rhel/install_fish.ts";
+import { installGit } from "../rhel/install_git.ts";
 
 export class RHELlInstaller extends BaseInstaller {
   override async installFish(): Promise<void> {
     await setupFish();
+  }
+  override async installGit(): Promise<void> {
+    await installGit();
   }
 }
