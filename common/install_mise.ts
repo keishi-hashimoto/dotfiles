5506016ad27f03b07d16329033ef310885e77be7
keishi hashimoto <keishi04hrikzira@gmail.com>
2025-05-18 07:53:00 -0400

    💪 共通処理を切り出したのに合わせて mise インストール処理をリファクタリング

--- common/install_mise.ts
+++ common/install_mise.ts
@@ -1,88 +1,24 @@
 import $ from "../utils/prompt.ts";
-import { CommandBuilder, type MultiSelectOption } from "@david/dax";
-import { shells } from "../constants.ts";
+import { SupportedShell } from "../constants.ts";
 import { Logger } from "../utils/logger.ts";
-import { getCurrentShell } from "../utils/shell.ts";
-
-import { isWritten } from "../utils/shell.ts";
+import { activate } from "./utils.ts";
 
 const logPrefix = "install mise";
 const logger = new Logger(logPrefix);
 
-const getTargetShellCandidates = (
-  curerntShell: string,
-): MultiSelectOption[] => {
-  return shells.map((s) => {
-    return { text: s, selected: s === curerntShell };
-  });
-};
-
-const getTargetShells = async (): Promise<string[]> => {
-  const curerntShell = await getCurrentShell();
-  const targetShellCandidates = getTargetShellCandidates(curerntShell);
-  const targetShellsIdx = await $.multiSelect({
-    message:
-      `In which shells to activate mise. (current shell "${curerntShell}" is selected by default.)`,
-    options: targetShellCandidates,
-    noClear: true,
-  });
-  const targetShells = targetShellCandidates.filter((_, idx) =>
-    targetShellsIdx.includes(idx)
-  ).map((s) => s.text);
-  logger.info(`Following shells are selected: ${targetShells}`);
-  return targetShells;
-};
-
-const getActivationTargetFileAndKeyword = (shell: string): [string, string] => {
-  let file = "";
-  let keyword = "";
-  const homeDir = Deno.env.get("HOME");
+const getActivationKeyword = (shell: SupportedShell): string => {
   switch (shell) {
     case "bash":
-      // ~ だと何故か path.exists() が動作しないので絶対パスで置き換える
-      file = `${homeDir}/.bashrc`;
-      keyword = `eval "$(~/.local/bin/mise activate bash)"`;
-      break;
+      return `eval "$(~/.local/bin/mise activate bash)"`;
     case "zsh":
-      file = `${homeDir}/.zshrc`;
-      keyword = `eval "$(~/.local/bin/mise activate zsh)"`;
-      break;
+      return `eval "$(~/.local/bin/mise activate zsh)"`;
     case "fish":
-      file = `${homeDir}/.config/fish/config.fish`;
-      keyword = `~/.local/bin/mise activate fish | source`;
+      return `~/.local/bin/mise activate fish | source`;
   }
-  return [file, keyword];
 };
 
 const activateMise = async () => {
-  const targetShells = await getTargetShells();
-  for (const shell of targetShells) {
-    const [file, keyword] = getActivationTargetFileAndKeyword(shell);
-    logger.info(
-      `check if activation keyword (${keyword}) is already written in file (${file})`,
-    );
-    // 後続の noThrow で全てのエラーが無視されるので、ファイルの有無は事前に確認しておく
-    const isExists = await $.path(file).exists();
-    if (isExists) {
-      const _isWritten = await isWritten(file, keyword);
-      if (_isWritten) {
-        logger.info(
-          `activation keyword ${keyword} is already written in file ${file}.`,
-        );
-        continue;
-      }
-    }
-    logger.info(`write activation keyword "${keyword}" to file ${file}.`);
-    // .rc ファイルが確実に存在するように、当該のシェルを経由させる
-    // 以下のやり方だと bash -c "echo "keyword" >> ~/.bashrc" のようになり "" が足りないので、CommandBuilder を使う
-    // await $`${shell} -c "echo ${keyword} >> ${$.path(file)}"`;
-    await new CommandBuilder().command([
-      shell,
-      "-c",
-      `echo ${keyword} >> ${file}`,
-    ]);
-    logger.info(`activation keyword is added to file ${file}.`);
-  }
+  await activate(getActivationKeyword);
 };
 
 export const installMise = async () => {
